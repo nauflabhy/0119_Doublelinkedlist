8e783cc99519832e2255fb82dd3da19ae6ede3bc
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-15 16:06:20 +0700

    Step 2a: START = START.next

--- Doublelikedlist.cpp
+++ Doublelikedlist.cpp
@@ -105,6 +105,10 @@ class DoubleLinkedList {
             // Step 2 : If node is at the beginning
             if (current == START)
             {
+                START = current->next; // Step 2a: START = START.next
+                if (START != NULL)
+                {
+                }
             }
 
         }
