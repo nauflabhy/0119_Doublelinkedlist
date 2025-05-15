bca074f52ef66b7e82217f16ce2d0a610d8fca28
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-15 17:12:33 +0700

    Step 3: Move to next node

--- Doublelikedlist.cpp
+++ Doublelikedlist.cpp
@@ -142,9 +142,9 @@ class DoubleLinkedList {
             {
                 cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
 
-                
+                // Step 3: Move to next node
+                currentNode = currentNode->next;
+                i++;
             }
-
-
         }
 };
